18cd8d247cbf00041e3623dda2c321cf90da3c05
JP <jp@jaypenner.com>
2024-02-24 20:51:20 -0800

    basic setups

---
+++ ses/ses-standard-expiry.md
@@ -0,0 +1,23 @@
+# A date's due!
+
+Heads-up!
+
+Your item: {{documentName}} is due in {{dueInDays}}, on {{dueDate}}. We thought you should know!
+
+Additional notes you entered: {{additionalNotes}}
+
+{{referenceLinks}}
+
+We hope that helped you!
+
+Note:
+
+Just head over to [https://onwhichdate.com](https://onwhichdate.com) to make any changes
+
+Thanks,
+
+from your friendly helpers @ owd.
+
+-----
+
+{{sponsoredLinks}}
--- utils/ses.js
+++ utils/ses.js
@@ -1,45 +1,44 @@
 #!/usr/bin/env node
 
 const { program } = require("commander");
-const fs = require('fs');
-const path = require('path');
-const { JSDOM }  = require('jsdom');
-const { marked }  = require('marked');
+const fs = require("fs");
+const path = require("path");
+const { JSDOM } = require("jsdom");
+const { marked } = require("marked");
 
 program
-  .option('-f, --file <type>', 'Specify the .md file')
-  .option('-a, --action <type>', 'Specify the action (template)')
-  .option('-n, --name <type>', 'Specify the template name')
-  .option('-s, --subj <type>', 'Specify the subject')
+  .option("-f, --file <type>", "Specify the .md file")
+  .option("-a, --action <type>", "Specify the action (template)")
+  .option("-n, --name <type>", "Specify the template name")
+  .option("-s, --subj <type>", "Specify the subject")
   .parse(process.argv);
 
 const options = program.opts();
 
 // Validate file extension
-if (path.extname(options.file) !== '.md') {
-  console.error('Error: Input file must be a .md file.');
+if (path.extname(options.file) !== ".md") {
+  console.error("Error: Input file must be a .md file.");
   process.exit(1);
 }
 
 // Read file content
-const fileContent = fs.readFileSync(options.file, 'utf8');
+const fileContent = fs.readFileSync(options.file, "utf8");
 
-if (options.action === 'template') {
+if (options.action === "template") {
+  let html = marked(fileContent);
 
-let html = marked(fileContent);
-
-    // Parse HTML content and extract text
- const dom = new JSDOM(html);
- const textContent = dom.window.document.body.textContent || '';
+  // Parse HTML content and extract text
+  const dom = new JSDOM(html);
+  const textContent = dom.window.document.body.textContent || "";
 
   // Create SES template
   const sesTemplate = {
-    "Template": {
-    TemplateName: options.name || "Your Template Name Here",
-    SubjectPart: options.subj || "Your Subject Here",
-    TextPart: textContent,
-    HtmlPart: html
-    }
+    Template: {
+      TemplateName: options.name || "Your Template Name Here",
+      SubjectPart: options.subj || "Your Subject Here",
+      TextPart: textContent,
+      HtmlPart: html,
+    },
   };
 
   // Print the SES template to the terminal
