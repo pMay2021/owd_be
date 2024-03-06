fd51d7dd52ae575f97703c0bd7f069df4efaa17d
JP <jp@jaypenner.com>
2024-03-05 19:42:08 -0800

    lambda-process-notice works including pulling ref data from s3

---
+++ dynamoDB/data/travel#us#passport.md
@@ -0,0 +1,5 @@
+Here are a few useful links:
+
+- **US Department of State Passport Page:** [https://travel.state.gov/content/travel/en/passports.html](https://travel.state.gov/content/travel/en/passports.html)
+
+- **Passport Processing Times:** [https://travel.state.gov/content/travel/en/passports/how-apply/processing-times.html](https://travel.state.gov/content/travel/en/passports/how-apply/processing-times.html)
--- lamdba/lambda-process-notices.js
+++ lamdba/lambda-process-notices.js
@@ -3,17 +3,19 @@
  *
  * change log
  * ----------
- * v1.0.3 basic version works on email
+ * v1.0.3 sends an email including adding markdown content for reference links
  */
 
 import * as owd from "/opt/nodejs/node20/owd.mjs";
 import * as db from "/opt/nodejs/node20/owddb.mjs";
 import * as ch from "/opt/nodejs/node20/channels.mjs";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+const s3 = new S3Client({});
 
 const goModify = true; //set to true to enable actual db operations
 
 const sendNotice = async (notice) => {
-  owd.log(notice, "\nSending notice for:", true);
+  owd.log(notice, "\nSending notice for:", false);
 
   // get the document details
   const docDetails = notice.documentId.S.split("|");
@@ -28,6 +30,12 @@ const sendNotice = async (notice) => {
   ch.setModify(true);
 
   const inDays = owd.getFriendlyDateDifference(new Date(), notice.originalExpiryOn.S);
+  //const fileKey = "travel#us#passport.md";
+  const fileKey = "db/" + docDb.pk.S + "#" + docDb.sk.S + ".md";
+  owd.log(fileKey, "The S3 file key");
+
+  const refContent = await db.getS3Text("onwhichdate", fileKey);
+  owd.log(refContent, "s3 text");
 
   const d = {
     name: customerDb.nickName.S,
@@ -35,7 +43,7 @@ const sendNotice = async (notice) => {
     documentName: docDb.documentName.S,
     dueInDays: inDays.sentence,
     dueDate: owd.getFriendlyDate(notice.originalExpiryOn.S),
-    referenceLinks: docDb.referenceURL.S,
+    referenceLinks: owd.getHtmlFromMarkdown(refContent),
   };
 
   owd.log(d, "\n send obj");
@@ -54,14 +62,14 @@ export const handler = async (event, context) => {
     const date = new Date();
     const hour = date.getHours().toString().padStart(2, "0");
     let pk = date.toISOString().split("T")[0] + "#" + hour;
-    pk = "2038-12-24#04"; //for testing purposes
+    pk = "2029-07-18#04"; //for testing purposes
 
     // get all records from the db-notice table that are due to be sent
     const notices = await db.queryItems("db-notice", "pk", "pk = :pk", { ":pk": { S: pk } });
 
     // loop through the notices and send them
     for (const notice of notices) {
-      owd.log(notice, "\nRetrieved notice details:", false);
+      owd.log(notice, "\nRetrieved notice details:", true);
       await sendNotice(notice);
     }
   } catch (error) {
