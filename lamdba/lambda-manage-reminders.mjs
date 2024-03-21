b65055a881fd10554fb68b7cd94703cdc13e6dc7
JP <jp@jaypenner.com>
2024-03-20 20:26:36 -0700

    cleaned up reminders, removed unused files

--- lamdba/lambda-manage-reminders.mjs
+++ lamdba/lambda-manage-reminders.mjs
@@ -1,31 +1,36 @@
-/*
- * this code manages (add, delete, update) notices
- * add: when customer adds a document, notices are created for the expiry dates
- * delete: when customer deletes a document, all notices are deleted, or deletes a specific notice
- * update: when customer updates a specific notice
- * We do not support update of the expiry date of the document, as that would require a cascade update of all notices
- *
- * change log
- * ----------
- * v1.0.7 - cleanups, authorizer mgmt. add, delete, update, and get work
- * v1.0.6 - now with new db layer with PartiQL, support for get
- * v1.0.5 - new layer, better error check on delete
- * v1.0.4 - delete and update code, change in schema; delete and update work
- * v1.0.3 - logging improvements and minor fixes, notice additions work; add works, need to extend to commands with add, delete, update
- * v1.0.2 - layer updated, code updated for revised schema
- * v1.0.1 basic version works through test and POST
- */
-
 import * as owd from "/opt/nodejs/node20/owd.mjs";
 import * as db from "/opt/nodejs/node20/owddb.mjs";
-const goModify = process.env.DB_MODIFY === "TRUE" ? true : false;
-const logLevel = process.env.LOG_LEVEL || "DEBUG"; //set to true to enable actual db operations
+
+/*
+ * This code manages (add, delete, update) notices.
+ * Add: when a customer adds a document, notices are created for the expiry dates.
+ * Delete: when a customer deletes a document, all notices are deleted, or deletes a specific notice.
+ * Update: when a customer updates a specific notice.
+ * We do not support updating the expiry date of the document, as that would require a cascade update of all notices.
+ *
+ * Change log:
+ * ----------
+ * v1.0.8 - additional cleanup, minor fixes.
+ * v1.0.7 - cleanups, authorizer management. Add, delete, update, and get work.
+ * v1.0.6 - now with new db layer with PartiQL, support for get.
+ * v1.0.5 - new layer, better error check on delete.
+ * v1.0.4 - delete and update code, change in schema; delete and update work.
+ * v1.0.3 - logging improvements and minor fixes, notice additions work; add works, need to extend to commands with add, delete, update.
+ * v1.0.2 - layer updated, code updated for revised schema.
+ * v1.0.1 - basic version works through test and POST.
+ */
+
+
+const goModify = process.env.DB_MODIFY === "TRUE";
+const logLevel = process.env.LOG_LEVEL || "DEBUG"; // Set to true to enable actual db operations
 
 export const handler = async (event, context) => {
   const command = event.pathParameters?.proxy;
 
   let obj = event.requestContext.authorizer.lambda.item;
-  if (!command) return owd.Response(400, "Missing/Invalid command");
+  if (!command) {
+    return owd.Response(400, "Missing/Invalid command");
+  }
 
   const hour = new Date().getHours().toString().padStart(2, "0");
   const runDisplay = {
@@ -44,22 +49,28 @@ export const handler = async (event, context) => {
   try {
     if (command === "add") {
       if (!cid || !expiresOn) {
-        throw new Error("Missing required parameters. (cid)");
+        throw new Error("Missing required parameters (cid or expiresOn)");
       }
 
       const customerDb = await db.getItem("db-customer", cid);
-      if (!customerDb) return owd.Response(404, { message: "Customer not found", email: email });
+      if (!customerDb) {
+        return owd.Response(404, { message: "Customer not found", email: email });
+      }
 
-      owd.debug(customerDb, "Retrieved customer details:");
+      owd.debug(customerDb, "Retrieved customer details");
 
-      let [dpk, dsk] = docId.split("|");
+      const [dpk, dsk] = docId.split("|");
       const docDb = await db.getItem("db-document", dpk, dsk);
       if (!docDb) {
         return owd.Response(404, { message: "Document not found", docId: dpk + " " + dsk });
       }
-      owd.debug(docDb, "Retrieved document details:");
+      owd.debug(docDb, "Retrieved document details");
 
-      const offsetArray = docDb.reminderOffsetDays.L.map((x) => x.N);
+      const offsetArray = docDb.reminderOffsetDays?.L?.map((x) => x?.N);
+      if (!offsetArray || offsetArray.length === 0) {
+        throw new Error("Invalid reminderOffsetDays");
+      }
+
       const newDates = await createAndInsertNotices(offsetArray, docDb);
       const retJson = { notices: newDates };
       const nd = newDates?.map((m) => m.date.date + " | " + m.date.dueInDays);
@@ -68,36 +79,43 @@ export const handler = async (event, context) => {
     }
 
     if (command === "delete") {
-      //delete a specific notice
       const { id } = event.queryStringParameters;
-      if (!id) return owd.Response(400, "Missing/Invalid id");
-
-      const noticeDb = await db.getItem("db-notices", id);
-      if (!noticeDb) return owd.Response(404, { message: "Notice not found", id: id });
-
-      if (cid != noticeDb.cid.S) {
-        return owd.Response(404, { message: "Customer/notice mismatch; verification failed " });
+      if (!id) {
+        return owd.Response(400, "Missing/Invalid id");
       }
 
-      //TODO if it's a parent, then we delete all children
+      const noticeDb = await db.getItem("db-notices", id);
+      if (!noticeDb) {
+        return owd.Response(404, { message: "Notice not found", id: id });
+      }
+
+      if (cid !== noticeDb.cid?.S) {
+        return owd.Response(404, { message: "Customer/notice mismatch; verification failed" });
+      }
+
+      // TODO: if it's a parent, then we delete all children
 
       if (goModify) {
         const resp = await db.deleteItem("db-notices", id);
-        owd.info(resp, "Deleted notice from db:");
+        owd.info(resp, "Deleted notice from db");
       } else {
-        owd.debug("Skipping actual db operation", "");
+        owd.debug("Skipping actual db operation");
       }
-      return owd.Response(200, { message: "Deleted notice: ", id: id });
+      return owd.Response(200, { message: "Deleted notice", id: id });
     }
 
     if (command === "update") {
       const { id } = event.queryStringParameters;
-      if (!id) return owd.Response(400, { message: "Missing/Invalid id", id: id });
+      if (!id) {
+        return owd.Response(400, { message: "Missing/Invalid id", id: id });
+      }
 
       const noticeDb = await db.getItem("db-notices", id);
-      if (!noticeDb) return owd.Response(404, { message: "Missing/Invalid notice", id: id });
+      if (!noticeDb) {
+        return owd.Response(404, { message: "Missing/Invalid notice", id: id });
+      }
 
-      const daysRemainingObj = owd.getFriendlyDateDifference(body.date, noticeDb.originalExpiryOn.S);
+      const daysRemainingObj = owd.getFriendlyDateDifference(body.date, noticeDb.originalExpiryOn?.S);
       owd.log(daysRemainingObj, "daysRemainingObj");
 
       const noticeItem = {
@@ -109,41 +127,43 @@ export const handler = async (event, context) => {
         notes: { S: body.notes },
       };
 
-      owd.debug(noticeItem, "constructed noticeItem");
+      owd.debug(noticeItem, "Constructed noticeItem");
 
       if (goModify) {
         const resp = await db.updateItem("db-notices", noticeItem, id);
-        return owd.Response(200, "successfully updated record");
+        return owd.Response(200, "Successfully updated record");
       } else {
-        owd.debug("Skipping actual db operation", "");
-        return owd.Response(200, "skipped update/but successful operation");
+        owd.debug("Skipping actual db operation");
+        return owd.Response(200, "Skipped update, but successful operation");
       }
     }
 
     if (command === "get") {
-      //TODO for now we don't limit reads, we'll pull all parent notices
+      // TODO: for now, we don't limit reads, we'll pull all parent notices
       const { n } = event.queryStringParameters;
       const items = await db.getParentNoticesByCid(cid);
-      if (!items || items.Count === 0) return owd.Response(404, "No data for customer");
+      if (!items || items.Count === 0) {
+        return owd.Response(404, "No data for customer");
+      }
       const ret = items
         ?.map((n) => {
           const i = {
-            doc: n.documentId.S,
-            expires: n.originalExpiryOn.S,
-            dueIn: n.dueInDays.N,
-            id: n.pk.S,
-            parentId: n.parentKey.S,
-            type: n.type.S,
+            doc: n.documentId?.S,
+            expires: n.originalExpiryOn?.S,
+            dueIn: n.dueInDays?.N,
+            id: n.pk?.S,
+            parentId: n.parentKey?.S,
+            type: n.type?.S,
           };
 
           return i;
         })
-        .filter((f) => f.type == "notice");
+        .filter((f) => f.type === "notice");
 
       return owd.Response(200, ret);
     }
 
-    return owd.Response(404, { message: "unknown command", event: event });
+    return owd.Response(404, { message: "Unknown command", event: event });
   } catch (error) {
     owd.error(error.message);
     const obj = {
@@ -161,9 +181,11 @@ export const handler = async (event, context) => {
     let parentKey = "";
 
     for (const date of newDates) {
-      const slot = date.date + "#" + hour + "#" + type; //e.g., 2028_11_28#21#notice; with this we can only query notices by type and slot
+      const slot = date.date + "#" + hour + "#" + type; // e.g., 2028_11_28#21#notice; with this, we can only query notices by type and slot
       const pk = owd.getShortId(25);
-      if (date.display.isToday && date.dueInDays == "0") parentKey = pk;
+      if (date.display.isToday && date.dueInDays === "0") {
+        parentKey = pk;
+      }
 
       const noticeItem = {
         pk: { S: pk },
@@ -175,7 +197,7 @@ export const handler = async (event, context) => {
         addedOn: { S: new Date().toISOString() },
         isParent: { BOOL: date.dueInDays === "0" },
         originalExpiryOn: { S: newDates[0].date },
-        documentId: { S: docDb.pk.S + "|" + docDb.sk.S },
+        documentId: { S: docDb.pk?.S + "|" + docDb.sk?.S },
         sendSMS: { BOOL: body.sendSMS },
         sendEmail: { BOOL: body.sendEmail },
         sendPush: { BOOL: body.sendPush },
@@ -185,12 +207,12 @@ export const handler = async (event, context) => {
         dueInDays: { N: date.dueInDays },
       };
 
-      owd.log(noticeItem, "Constructed notice entry for:");
+      owd.log(noticeItem, "Constructed notice entry for");
       if (goModify) {
         const resp = await db.putItem("db-notices", noticeItem);
-        owd.info(resp, "Added notice to db:");
+        owd.info(resp, "Added notice to db");
       } else {
-        owd.debug("Skipping actual db operation", "");
+        owd.debug("Skipping actual db operation");
       }
 
       ret.push({
