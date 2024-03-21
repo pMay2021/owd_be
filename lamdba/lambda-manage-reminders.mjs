a09f1a8162f38787ecb357ae5b2a585bdc0bfe1d
JP <jp@jaypenner.com>
2024-03-20 20:19:21 -0700

    reminder mgmt all basic functions work

--- lamdba/lambda-manage-reminders.mjs
+++ lamdba/lambda-manage-reminders.mjs
@@ -7,6 +7,7 @@
  *
  * change log
  * ----------
+ * v1.0.7 - cleanups, authorizer mgmt. add, delete, update, and get work
  * v1.0.6 - now with new db layer with PartiQL, support for get
  * v1.0.5 - new layer, better error check on delete
  * v1.0.4 - delete and update code, change in schema; delete and update work
@@ -22,6 +23,8 @@ const logLevel = process.env.LOG_LEVEL || "DEBUG"; //set to true to enable actua
 
 export const handler = async (event, context) => {
   const command = event.pathParameters?.proxy;
+
+  let obj = event.requestContext.authorizer.lambda.item;
   if (!command) return owd.Response(400, "Missing/Invalid command");
 
   const hour = new Date().getHours().toString().padStart(2, "0");
@@ -35,30 +38,28 @@ export const handler = async (event, context) => {
   owd.info(runDisplay, "Execution Variables");
   const body = JSON.parse(event.body);
   owd.info(body, "The incoming body");
-  const { cid, docId, expiresOn } = body;
+  const { email, cid } = obj;
+  const { docId, expiresOn } = body;
 
   try {
     if (command === "add") {
-      // Validate input parameters
       if (!cid || !expiresOn) {
-        throw new Error("Missing required parameters.");
+        throw new Error("Missing required parameters. (cid)");
       }
 
       const customerDb = await db.getItem("db-customer", cid);
-
-      if (!customerDb) return owd.Response(404, "Customer not found");
+      if (!customerDb) return owd.Response(404, { message: "Customer not found", email: email });
 
       owd.debug(customerDb, "Retrieved customer details:");
 
       let [dpk, dsk] = docId.split("|");
       const docDb = await db.getItem("db-document", dpk, dsk);
       if (!docDb) {
-        return owd.Response(404, "Document not found for " + dpk + " " + dsk);
+        return owd.Response(404, { message: "Document not found", docId: dpk + " " + dsk });
       }
       owd.debug(docDb, "Retrieved document details:");
 
       const offsetArray = docDb.reminderOffsetDays.L.map((x) => x.N);
-      console.debug("entering create and insert");
       const newDates = await createAndInsertNotices(offsetArray, docDb);
       const retJson = { notices: newDates };
       const nd = newDates?.map((m) => m.date.date + " | " + m.date.dueInDays);
@@ -68,17 +69,17 @@ export const handler = async (event, context) => {
 
     if (command === "delete") {
       //delete a specific notice
-
-      const { id, email } = event.queryStringParameters;
-      if (!id || !email) return owd.Response(400, "Missing/Invalid id and/or email");
+      const { id } = event.queryStringParameters;
+      if (!id) return owd.Response(400, "Missing/Invalid id");
 
       const noticeDb = await db.getItem("db-notices", id);
-      if (!noticeDb) return owd.Response(404, "Notice not found");
+      if (!noticeDb) return owd.Response(404, { message: "Notice not found", id: id });
 
-      const cdb = await db.getCustomerByEmail(email);
-      if (!cdb) return owd.Response(404, `Customer ${email} not found`);
+      if (cid != noticeDb.cid.S) {
+        return owd.Response(404, { message: "Customer/notice mismatch; verification failed " });
+      }
 
-      if (cdb.pk.S != noticeDb.cid.S) return owd.Response(404, "Customer/notice mismatch; verification failed " + cdb.pk.S + " " + noticeDb.cid.S);
+      //TODO if it's a parent, then we delete all children
 
       if (goModify) {
         const resp = await db.deleteItem("db-notices", id);
@@ -86,15 +87,15 @@ export const handler = async (event, context) => {
       } else {
         owd.debug("Skipping actual db operation", "");
       }
-      return owd.Response(200, "Deleted notice: " + id);
+      return owd.Response(200, { message: "Deleted notice: ", id: id });
     }
 
     if (command === "update") {
-      owd.debug(body, "now executing update with id=:" + body.id);
-      const id = body.id;
-      if (!id) return owd.Response(400, "Missing/Invalid id: " + id);
+      const { id } = event.queryStringParameters;
+      if (!id) return owd.Response(400, { message: "Missing/Invalid id", id: id });
+
       const noticeDb = await db.getItem("db-notices", id);
-      if (!noticeDb) return owd.Response(404, "Notice not found: " + id);
+      if (!noticeDb) return owd.Response(404, { message: "Missing/Invalid notice", id: id });
 
       const daysRemainingObj = owd.getFriendlyDateDifference(body.date, noticeDb.originalExpiryOn.S);
       owd.log(daysRemainingObj, "daysRemainingObj");
@@ -121,17 +122,17 @@ export const handler = async (event, context) => {
 
     if (command === "get") {
       //TODO for now we don't limit reads, we'll pull all parent notices
-      const { n, cid } = event.queryStringParameters;
+      const { n } = event.queryStringParameters;
       const items = await db.getParentNoticesByCid(cid);
-      owd.log(items, "items");
       if (!items || items.Count === 0) return owd.Response(404, "No data for customer");
       const ret = items
         ?.map((n) => {
-          owd.log(n, "iterating on n");
           const i = {
             doc: n.documentId.S,
             expires: n.originalExpiryOn.S,
             dueIn: n.dueInDays.N,
+            id: n.pk.S,
+            parentId: n.parentKey.S,
             type: n.type.S,
           };
 
@@ -154,16 +155,20 @@ export const handler = async (event, context) => {
 
   async function createAndInsertNotices(offsetArray, docDb) {
     const newDates = owd.getOffsetDates(expiresOn, offsetArray);
+
     const ret = [];
     const type = "notice";
+    let parentKey = "";
 
     for (const date of newDates) {
       const slot = date.date + "#" + hour + "#" + type; //e.g., 2028_11_28#21#notice; with this we can only query notices by type and slot
       const pk = owd.getShortId(25);
-      owd.log(date, "loop date");
+      if (date.display.isToday && date.dueInDays == "0") parentKey = pk;
+
       const noticeItem = {
         pk: { S: pk },
         cid: { S: cid },
+        parentKey: { S: parentKey },
         slot: { S: slot },
         type: { S: type },
         isAlreadySent: { BOOL: false },
