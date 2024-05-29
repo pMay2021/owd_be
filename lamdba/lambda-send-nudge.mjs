f2638c5b7e5bd1906c34aa1fb3779629e19ed8af
JP <jp@jaypenner.com>
2024-05-28 21:11:59 -0700

    with raw sending mode working in basic

--- lamdba/lambda-send-nudge.mjs
+++ lamdba/lambda-send-nudge.mjs
@@ -2,10 +2,15 @@
  
  * change log
  * ----------
- * v1.0.1 basic, with proper api mgmt.
+ * v0.1 this code works for sending simple and raw email with attachment
  */
 
-import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
+import {
+  SESv2Client,
+  SendEmailCommand,
+} from "@aws-sdk/client-sesv2";
+//import nodemailer from "nodemailer";
+
 const ses = new SESv2Client({ region: "us-east-1" });
 const devEmail = "m.venugopal@gmail.com";
 const fromEmail = "no-reply@eznudge.com";
@@ -16,9 +21,8 @@ const log = console.log;
  * Send an email using SES.
  * @param {string} fromAddress
  * @param {string} toAddress
- * @param {string} templateName
- * @param {object} templateData
- * @param {boolean} printLogs - Whether to print logs to the console.
+ * @param {array} list of cc addresses
+ * @param {string} subject of the email
  */
 const sendEmail = async (fromAddress, toAddress, cc, subject, content) => {
   log(`Sending email to: ${toAddress} from: ${fromAddress}`);
@@ -27,6 +31,7 @@ const sendEmail = async (fromAddress, toAddress, cc, subject, content) => {
     FromEmailAddress: fromAddress,
     Destination: {
       ToAddresses: [toAddress],
+      CcAddresses: cc?.length > 0 ? cc : [],
     },
     Content: {
       Simple: {
@@ -48,6 +53,62 @@ const sendEmail = async (fromAddress, toAddress, cc, subject, content) => {
   return sesResponse;
 };
 
+const sendRawEmail = async (sendParams) => {
+  log(`Sending raw email:\n ${JSON.stringify(sendParams)}`);
+
+  const boundary = "----=_NextPart_001_0000";
+
+  const rawEmail = `
+From: ${sendParams.from}
+To: ${sendParams.to}
+Subject: ${sendParams.subject}
+Cc: ${sendParams.cc?.length > 0 ? sendParams.cc.join(", ") : ""},
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="${boundary}"
+
+--${boundary}
+Content-Type: text/plain; charset=UTF-8
+
+Let us send a dummy content
+
+--${boundary}
+Content-Type: text/calendar; name="eznudge.ics"
+Content-Description: eznudge.ics
+Content-Disposition: attachment; filename="eznudge.ics"
+Content-Transfer-Encoding: base64
+
+${sendParams.attachmentContentBase64}
+
+--${boundary}--
+`;
+
+//let data = Buffer.from(rawEmail.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n"), 'utf8');
+let data = Buffer.from(rawEmail.trim(), 'utf8');
+
+  const params = {
+    FromEmailAddress: sendParams.from,
+    Destination: {
+      ToAddresses: [sendParams.to],
+      CcAddresses: sendParams.cc?.length > 0 ? sendParams.cc : [],
+    },
+    Content: {
+       Raw: {
+        Data: data
+      },
+    },
+  };
+  
+
+  try {
+    const sesResponse = await ses.send(new SendEmailCommand(params));
+    console.log("Email sent successfully:", sesResponse);
+    return Response(200, "Email sent successfully", "Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return Response(500, "Error sending email", error.message);
+  }
+};
+
 function Response(code, title = "response", message = "none") {
   const body = {
     title: title,
@@ -64,8 +125,6 @@ function Response(code, title = "response", message = "none") {
 }
 
 export const handler = async (event, context) => {
-  console.log("received event: ", event);
-
   try {
     const body = JSON.parse(event.Records[0].body);
     log("received body: ", body);
@@ -76,14 +135,14 @@ export const handler = async (event, context) => {
     }
 
     const email = body.to.trim()?.toLowerCase();
-    log("ready to send to " + email + "with content " + body.content);
-    const r = await sendEmail(
-      "no-reply@eznudge.com",
-      devEmail,
-      "",
-      body.subject,
-      body.content
-    );
+    const r = await sendRawEmail(body);
+    // const r = await sendEmail(
+    //   "no-reply@eznudge.com",
+    //   devEmail,
+    //   body.cc,
+    //   body.subject,
+    //   body.content
+    // );
     console.log(r);
     return Response(202, "Email sent.", "email sent");
   } catch (error) {
